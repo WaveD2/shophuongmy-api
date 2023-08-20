cd6b717c070abc6c815557b24e1feed9e8babdb0
WaveD <109639330+WaveD2@users.noreply.github.com>
2023-08-20 18:34:19 +0700

    V1

---
+++ src/Routers/ProductRouter.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const ProductController = require("../Controllers/ProductController");
+const {
+  authMiddleware,
+  authUserMiddleware,
+} = require("../middleware/authMiddleware");
+
+router.get("/get-all", authUserMiddleware, ProductController.getAllProduct);
+router.get(
+  "/get-details/:id",
+  authUserMiddleware,
+  ProductController.getDetailsProduct
+);
+
+router.get("/type/:type", authUserMiddleware, ProductController.getProductType);
+router.get("/get-all-type", authUserMiddleware, ProductController.getAllType);
+router.post("/create", authMiddleware, ProductController.createProduct);
+router.post("/delete-many", authMiddleware, ProductController.deleteMany);
+router.put("/update/:id", authMiddleware, ProductController.updateProduct);
+router.delete("/delete/:id", authMiddleware, ProductController.deleteProduct);
+
+module.exports = router;
---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,48 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+  if (!req.headers?.token) {
+    return res.status(403).json({
+      message: "Error token middleware",
+    });
+  }
+
+  const token = req.headers.token.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
+    if (err || !user?.isAdmin) {
+      return res.status(400).json({
+        message: "Error authMiddleWare",
+      });
+    }
+    if (user?.isAdmin) {
+      next();
+    }
+  });
+};
+
+const authUserMiddleware = (req, res, next) => {
+  if (!req.headers?.token) {
+    return res.status(403).json({
+      message: "Error token middleware",
+    });
+  }
+  const token = req.headers.token.split(" ")[1];
+  const userID = req.params.id;
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
+    if (err) {
+      return res.status(400).json({
+        message: "Error access_token expired",
+      });
+    } else if (user?.isAdmin || user?.id === userID) {
+      next();
+    } else if (user?.id !== userID) {
+      return res.status(400).json({
+        message: "Error information",
+      });
+    }
+  });
+};
+
+module.exports = { authMiddleware, authUserMiddleware };
