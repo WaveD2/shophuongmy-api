363eb6d698545c3acb05470809c7f8bd8cd6609e
WaveD2 <tungdev64@gmail.com>
2024-01-20 16:54:00 +0700

    update chatbot v1.2.0

--- src/Controllers/ServicesController.js
+++ src/Controllers/ServicesController.js
@@ -1,14 +1,26 @@
-const getWebhookFB = async (req, res) => {
-  return res.status(200).send("get thành công");
-};
 const postWebhookFB = async (req, res) => {
   let body = req.body;
-
-  console.log(`\u{1F7EA} Received webhook:`, body);
-  console.dir(body, { depth: null });
-
   if (body.object === "page") {
-    // Returns a '200 OK' response to all requests
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function (entry) {
+      // Gets the body of the webhook event
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log("Sender PSID: " + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
+    });
+
+    // Return a '200 OK' response to all events
     res.status(200).send("EVENT_RECEIVED");
   } else {
     // Return a '404 Not Found' if event is not from a page subscription
@@ -36,8 +48,51 @@ const getMessagingWebhookFB = (req, res) => {
   }
 };
 
+let handlePostback = (sender_psid, received_postback) => {
+  let response;
+
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+
+  // Set the response based on the postback payload
+  if (payload === "yes") {
+    response = { text: "Thanks!" };
+  } else if (payload === "no") {
+    response = { text: "Oops, try sending another image." };
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
+};
+
+// Sends response messages via the Send API
+let callSendAPI = (sender_psid, response) => {
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    message: response,
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request(
+    {
+      uri: "https://graph.facebook.com/v6.0/me/messages",
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+};
+
 module.exports = {
   postWebhookFB,
   getMessagingWebhookFB,
-  getWebhookFB,
 };
